ecc0e0f9c61a0ced449f503a4e0686bf307ea2cd
sbasu107 <40325803+sbasu107@users.noreply.github.com>
2025-01-14 15:10:24 -0800

    Got respinse from Gemini and saved it into a text file, updated README

--- scraper.py
+++ scraper.py
@@ -1,11 +1,25 @@
+import os
 import requests
 from bs4 import BeautifulSoup
+import google.generativeai as genai
 
 website_url = 'https://samplearticlescraping.web.app'
-response = requests.get(website_url)
+genai.configure(api_key = os.getenv("GOOGLE_API_KEY"))
 
-soup = BeautifulSoup(response.text, 'html.parser')
+requests_response = requests.get(website_url)
+
+#Clean response using BeautifulSoup
+soup = BeautifulSoup(requests_response.text, 'html.parser')
 article_contents = soup.get_text()
 
-print(article_contents)
+question = "Explain the benefits of tofu that the following article points out."
 
+#Set Gemini model and feed input here
+model = genai.GenerativeModel('gemini-1.5-pro')
+llm_response = model.generate_content(question + " : " + article_contents, stream=True)
+
+for chunk in llm_response:
+    print(chunk.text, end='')
+
+with open("scrapingProj/response.txt", "w") as file:
+    file.write(llm_response.text)
